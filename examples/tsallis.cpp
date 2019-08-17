cb5b397cf75e34aea687145e3a865487028a0fb9
Tom Westerhout <t.westerhout@student.ru.nl>
2019-08-18 01:52:40 +0200

    Use floats for Tsallis distribution parameters since we don't get any extra accuracy from doubles

--- examples/tsallis.cpp
+++ examples/tsallis.cpp
@@ -27,66 +27,46 @@ auto open_output_file(std::string const& filename) -> file_handler_t
 
 /// Reads q_V, t_V, and output file from `argv`.
 auto parse_arguments(int argc, char* argv[])
-    -> std::tuple<double, double, file_handler_t>
+    -> std::tuple<float, float, file_handler_t>
 {
     if (argc != 4) {
         std::fprintf(stderr, "Usage: %s <q_V> <t_V> <filename>\n", argv[0]);
         std::exit(1);
     }
-    auto const read_double = [](auto* s) {
+    auto const read_real = [](auto* s) {
         char*      p = nullptr;
-        auto const x = std::strtod(s, &p);
+        auto const x = std::strtof(s, &p);
         if (p == s) {
             std::fprintf(stderr, "Failed to interpret \"%s\" as double\n", s);
             std::exit(1);
         }
         return x;
     };
-    auto const q_V = read_double(argv[1]);
-    auto const t_V = read_double(argv[2]);
+    auto const q_V = read_real(argv[1]);
+    auto const t_V = read_real(argv[2]);
     auto       out = open_output_file(argv[3]);
     return {q_V, t_V, std::move(out)};
-}
-
-// Logarithm of eq. (2) from [@Schanze2006] for D=1.
-//
-// [@Schanze2006]: Thomas Schanze, "An exact D-dimensional Tsallis random
-//                 number generator for generalized simulated annealing",
-//                 2006.
-auto exact_log_prob(double const q_V, double const t_V)
-{
-    // This is all not performance critical, so no need to optimise it at all
-    auto const normalisation = std::sqrt((q_V - 1.0) / M_PI)
-                               * std::tgamma(1.0 / (q_V - 1.0))
-                               / std::tgamma(1.0 / (q_V - 1.0) - 0.5)
-                               * std::pow(t_V, -1.0 / (3.0 - q_V));
-    return [q_V, t_V, normalisation](auto const x) {
-        return std::log(
-            normalisation
-            / std::pow(
-                1.0 + (q_V - 1.0) * x * x / std::pow(t_V, 2.0 / (3.0 - q_V)),
-                1.0 / (q_V - 1.0)));
-    };
 }
 } // namespace
 
 auto main(int argc, char* argv[]) -> int
 {
     auto [q_V, t_V, out] = parse_arguments(argc, argv);
-    if (q_V <= 1.0 || q_V >= 3.0) {
+    if (q_V <= 1.0f || q_V >= 3.0f) {
         std::fprintf(stderr, "Invalid q_V: %f; expected 1.0 < q_V < 3.0\n",
-                     q_V);
+                     static_cast<double>(q_V));
         std::exit(1);
     }
-    if (t_V <= 0.0) {
-        std::fprintf(stderr, "Invalid t_V: %f; expected t_V > 0.0\n", t_V);
+    if (t_V <= 0.0f) {
+        std::fprintf(stderr, "Invalid t_V: %f; expected t_V > 0.0\n",
+                     static_cast<double>(t_V));
         std::exit(1);
     }
 
     pcg32                                  generator{12349827UL};
     dual_annealing::tsallis_distribution_t dist{q_V, t_V};
     auto const generate_sample = [&generator, &dist]() {
-        return static_cast<double>(dist.one(generator));
+        return static_cast<double>(dist(generator));
     };
 
     constexpr auto number_bins = 400;
@@ -104,13 +84,13 @@ auto main(int argc, char* argv[]) -> int
         process_sample(generate_sample());
     }
 
-    auto exact_log_dist = exact_log_prob(q_V, t_V);
+    auto exact = [p = dist.exact<1>()](auto const x) { return std::log(p(x)); };
     for (auto i = size_t{0}; i < number_bins; ++i) {
         auto const x = min + bin_size * (static_cast<double>(i) + 0.5);
         std::fprintf(out.get(), "%.5e\t%.5e\t%.5e\n", x,
                      std::log(static_cast<double>(bins[i])
                               / static_cast<double>(number_samples)),
-                     exact_log_dist(x));
+                     exact(x));
     }
     return 0;
 }
--- include/tsallis_distribution.hpp
+++ include/tsallis_distribution.hpp
@@ -4,24 +4,25 @@
 #include "config.hpp"
 
 #include <cmath> // std::sqrt, std::pow
-#include <cstddef>
 #include <random>
 
 DA_NAMESPACE_BEGIN
 
 struct tsallis_distribution_t {
   private:
+    using real_type = float;
+
     /// \brief Calculation of \f$p\f$ given \f$q_V\f$ (see `Tsallis_RNG`
     /// function in [@Schanze2006] for an explanation).
     ///
     /// [@Schanze2006]: Thomas Schanze, "An exact D-dimensional Tsallis random
     ///                 number generator for generalized simulated annealing",
     ///                 2006.
-    static constexpr auto get_p(double const q_V) noexcept -> double
+    static constexpr auto get_p(real_type const q_V) noexcept -> real_type
     {
-        DUAL_ANNEALING_ASSERT(1.0 < q_V && q_V < 3.0,
+        DUAL_ANNEALING_ASSERT(real_type{1} < q_V && q_V < real_type{3},
                               "`q_V` must be in (1, 3)");
-        return (3.0 - q_V) / (2.0 * (q_V - 1.0));
+        return (real_type{3} - q_V) / (real_type{2} * (q_V - real_type{1}));
     }
 
     /// \brief Calculation of \f$s\f$ given \f$q_V\f$ and \f$t_V\f$ (see
@@ -30,30 +31,31 @@ struct tsallis_distribution_t {
     /// [@Schanze2006]: Thomas Schanze, "An exact D-dimensional Tsallis random
     ///                 number generator for generalized simulated annealing",
     ///                 2006.
-    static /*constexpr*/ auto get_s(double const q_V, double const t_V) noexcept
-        -> double
+    static /*constexpr*/ auto get_s(real_type const q_V,
+                                    real_type const t_V) noexcept -> real_type
     {
-        return std::sqrt(2.0 * (q_V - 1.0)) / std::pow(t_V, 1.0 / (3.0 - q_V));
+        return std::sqrt(real_type{2} * (q_V - real_type{1}))
+               / std::pow(t_V, real_type{1} / (real_type{3} - q_V));
     }
 
   public:
     class param_type {
-        double _q_V; ///< Visiting distribution shape parameter
-        double _t_V; ///< Visiting temperature
-        double _s;   ///< Variable `s` from `Tsallis_RNG` in [@Schanze2006].
+        real_type _q_V; ///< Visiting distribution shape parameter
+        real_type _t_V; ///< Visiting temperature
+        real_type _s;   ///< Variable `s` from `Tsallis_RNG` in [@Schanze2006].
 
         // This is a hack to persuade Clang to generate constexpr `param()`
         // function for tsallis_distribution
-        constexpr param_type() noexcept : _q_V{0.0}, _t_V{0.0}, _s{0.0} {}
+        constexpr param_type() noexcept : _q_V{0}, _t_V{0}, _s{0} {}
 
       public:
-        explicit /*constexpr*/ param_type(double const q_V,
-                                          double const t_V) noexcept
+        explicit /*constexpr*/ param_type(real_type const q_V,
+                                          real_type const t_V) noexcept
             : _q_V{q_V}, _t_V{t_V}
         {
-            DUAL_ANNEALING_ASSERT(1.0 < q_V && q_V < 3.0,
+            DUAL_ANNEALING_ASSERT(real_type{1} < q_V && q_V < real_type{3},
                                   "`q_V` must be in (1, 3)");
-            DUAL_ANNEALING_ASSERT(t_V > 0, "`t_V` must be positive");
+            DUAL_ANNEALING_ASSERT(t_V > real_type{0}, "`t_V` must be positive");
             _s = get_s(q_V, t_V);
         }
 
@@ -77,14 +79,15 @@ struct tsallis_distribution_t {
             return *this;
         }
 
-        constexpr auto q_V() const noexcept { return _q_V; }
-        constexpr auto t_V() const noexcept { return _t_V; }
-        constexpr auto s() const noexcept { return _s; }
+        [[nodiscard]] constexpr auto q_V() const noexcept { return _q_V; }
+        [[nodiscard]] constexpr auto t_V() const noexcept { return _t_V; }
+        [[nodiscard]] constexpr auto s() const noexcept { return _s; }
     };
 
-    explicit tsallis_distribution_t(double const q_V, double const t_V) noexcept
-        : _gamma_dist{static_cast<float>(get_p(q_V)), 1.0f}
-        , _normal_dist{0.0f, 1.0f}
+    explicit tsallis_distribution_t(real_type const q_V,
+                                    real_type const t_V) noexcept
+        : _gamma_dist{get_p(q_V), real_type{1}}
+        , _normal_dist{real_type{0}, real_type{1}}
         , _params{q_V, t_V}
     {}
 
@@ -95,6 +98,9 @@ struct tsallis_distribution_t {
     auto operator                  =(tsallis_distribution_t&&) noexcept
         -> tsallis_distribution_t& = default;
 
+    [[nodiscard]] constexpr auto q_V() const noexcept { return _params.q_V(); }
+    [[nodiscard]] constexpr auto t_V() const noexcept { return _params.t_V(); }
+
     [[nodiscard]] constexpr auto param() const noexcept -> param_type
     {
         return _params;
@@ -102,31 +108,76 @@ struct tsallis_distribution_t {
 
     constexpr auto param(param_type const& params) noexcept -> void
     {
-        _gamma_dist.param(typename std::gamma_distribution<float>::param_type{
-            static_cast<float>(get_p(params.q_V())), 1.0});
+        if (_params.q_V() != params.q_V()) {
+            _gamma_dist.param(
+                typename std::gamma_distribution<real_type>::param_type{
+                    get_p(params.q_V()), real_type{1}});
+        }
         _params = params;
     }
 
-    template <class Generator> auto one(Generator& generator) noexcept -> float
+    template <class Generator>
+    auto operator()(Generator& generator) noexcept -> real_type
     {
         auto const u = _gamma_dist(generator);
-        auto const y = static_cast<float>(_params.s()) * std::sqrt(u);
+        auto const y = _params.s() * std::sqrt(u);
         auto const x = _normal_dist(generator);
         return x / y;
     }
 
-    template <class Generator> auto many(Generator& generator) const noexcept
+    template <class Generator> auto many(Generator& generator) noexcept
     {
         auto const u = _gamma_dist(generator);
-        auto const y = static_cast<float>(_params.s()) * std::sqrt(u);
-        return [normal_dist = std::normal_distribution<float>{0.0f, 1.0f / y},
-                &generator]() { return normal_dist(generator); };
+        auto const y = _params.s() * std::sqrt(u);
+        return [normal_dist =
+                    std::normal_distribution<real_type>{real_type{0},
+                                                        real_type{1} / y},
+                &generator]() mutable { return normal_dist(generator); };
+    }
+
+    template <int64_t D = -1> auto exact() const noexcept
+    {
+        static_assert(D > 0 || D == -1, "Invalid dimension");
+        auto const q        = static_cast<double>(q_V());
+        auto const t        = static_cast<double>(t_V());
+        auto const a        = (q - 1.0) * std::pow(t, -2.0 / (3.0 - q));
+        auto const scale_fn = [q, t](auto const d) {
+            auto const term_1 = std::pow((q - 1.0) / M_PI, d / 2.0);
+            auto const term_2 = std::tgamma(1.0 / (q - 1.0) + (d - 1.0) / 2.0)
+                                / std::tgamma(1.0 / (q - 1.0) - 0.5);
+            auto const term_3 = std::pow(t, d / (q - 3.0));
+            return term_1 * term_2 * term_3;
+        };
+        auto const b_fn = [q](auto const d) {
+            return 1.0 / (1.0 - q) + (1.0 - d) / 2.0;
+        };
+
+        if constexpr (D > 0) {
+            auto const _D    = static_cast<double>(D);
+            auto const scale = scale_fn(_D);
+            auto const b     = b_fn(_D);
+            return [scale, a, b](auto const x) {
+                return scale * std::pow(1.0 + a * x * x, b);
+            };
+        }
+        else {
+            return [a, scale_fn, b_fn](auto const x) {
+                using std::begin, std::end;
+                auto const _D    = static_cast<double>(x.size());
+                auto const scale = scale_fn(_D);
+                auto const b     = b_fn(_D);
+                auto const x_2   = std::accumulate(
+                    begin(x), end(x), 0.0,
+                    [](auto acc, auto const y) { return acc + y * y; });
+                return scale * std::pow(1.0 + a * x_2, b);
+            };
+        }
     }
 
   private:
-    std::gamma_distribution<float>  _gamma_dist;
-    std::normal_distribution<float> _normal_dist;
-    param_type                      _params;
+    std::gamma_distribution<real_type>  _gamma_dist;
+    std::normal_distribution<real_type> _normal_dist;
+    param_type                          _params;
 };
 
 DA_NAMESPACE_END
